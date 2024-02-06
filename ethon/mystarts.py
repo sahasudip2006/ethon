09eec49db0f690cbcce0ede1559a7680192090f3
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2024-02-06 19:56:02 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -6,6 +6,7 @@ from telethon import events, Button
 async def start_srb(event, st):
     await event.reply(st, 
                       buttons=[
+                              [Button.url("🧡Movie Request Group", url="https://t.me/Ott_Movie_Request_Group")],
                               [Button.inline("SET THUMB.", data="set"),
                                Button.inline("REM THUMB.", data="rem")],
                               [Button.url("DEV", url="t.me/SUDIPSAHA06")]])
@@ -14,9 +15,10 @@ async def start_srb(event, st):
 async def vc_menu(event):
     await event.edit("**VIDEO CONVERTOR v1.4**", 
                     buttons=[
+                        [Button.url("🧡Movie Request Group", url="https://t.me/Ott_Movie_Request_Group")],
                         [Button.inline("info.", data="info"),
                          Button.inline("SOURCE", data="source")],
                         [Button.inline("NOTICE.", data="notice"),
                          Button.inline("Main.", data="help")],
                         [Button.url("DEVELOPER", url="t.me/SUDIPSAHA06")]])
-    
+
